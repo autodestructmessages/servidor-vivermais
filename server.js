822f62a86452abc0ad3f0db7d6e54012e8b57754
autodestructmessages <silvonei.langenberg@escola.pr.gov.br>
2026-05-18 16:17:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -347,7 +347,7 @@ io.on('connection', socket => {
   const VERSAO_MINIMA_APP = '4.0.0';
 
   const LINK_NOVO_APK =
-    'https://drive.google.com/file/d/1arVwylK6sHcNty2EndHuU0lhURrofDo2/view?usp=sharing';
+    'https://drive.google.com/file/d/1KtvxtZeUpe_tHSpwwKvEICo4T5yQzesY/view?usp=sharing';
 
   socket.on('verificar_versao', (versaoApp, callback) => {
     if (versaoApp !== VERSAO_MINIMA_APP) {
